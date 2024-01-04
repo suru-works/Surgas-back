47a6ca2c1760fe49c58b0bf5b54ad438296d152d
Santiago Quiroz Upegui <squirozu@experimentality.co>
2024-01-04 10:24:53 -0500

    no message

---
+++ config/baseURL.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //local
+    dbUrl: "mongodb://localhost:27017/surgas",
+    appPort: 3001,
+    //PDN
+    //dbUrl: "mongodb://localhost:27017/surgas",
+    //appPort: 3001
+};
---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+const baseURL = require("../config/baseURL.js");
+const models = require("./models");
+const services = require("./services");
+const controllers = require("./controllers");
+
+app.use(express.json());
+
+app.use(services.auth.middleware);
+
+app.use(controllers.routes);
+
+app.listen(baseURL.appPort, () => {
+  console.log("El servidor está escuchando en el puerto 3000");
+});
